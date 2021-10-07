8f9c10ddaa56ed7acd191640f8edc62d1d5897f1
jie yang <jieyng@umich.edu>
2021-10-06 22:32:24 -0400

    added extra #

--- bash_exercise.sh
+++ bash_exercise.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 
-# ECHO command
+## ECHO command
 # echo hello world!
 
-# Variables
+## Variables
 # Uppercase by convention
 # Letters, numbers, undercase
 # NAME="jie yang1"
 # echo "My Name is: ${NAME}"
 # echo "My Name is: $NAME"
 
-# User Inputs
+## User Inputs
 # read -p "Enter your name: " NAME
 # echo "Hello ${NAME}, nice to meet you!"
 
-# Simple if statement
+## Simple if statement
 # if [ "${NAME}" == "jie yang" ]
 # then
 #     echo "Your name is jie yang"
 # fi
 
-# if-else
+## if-else
 # if [ "${NAME}" == "jie yang" ]
 # then
 #     echo "Your name is jie yang"
@@ -28,7 +28,7 @@
 #     echo "Your name is not jie yang"
 # fi
 
-# if-else-if-else
+## if-else-if-else
 # if [ "${NAME}" == "jie yang" ]
 # then
 #     echo "Your name is jie yang"
@@ -39,7 +39,7 @@
 #     echo "Your name is not jie yang or chang li"
 # fi
 
-# Comparison
+## Comparison
 # val1 -eq val2 Returns true if the values are equal
 # val1 -ne val2 Returns true if the values are not equal
 # val1 -gt val2 Returns true if the values greater than val2
@@ -58,7 +58,7 @@
 #     echo "${NUM1} is less than ${NUM2}"
 # fi
 
-# File conditions
+## File conditions
 # -d file True if the file is a directory
 # -e file True if the file exists (note that this is not particularly portable, thus -f is generally used)
 # -f file True if the provided string is a file
@@ -84,7 +84,7 @@
 #     echo "${FOLDER} does not exist"
 # fi
 
-#Case statment
+## Case statment
 # read -p "Are you 21 or over? Y/N " ANSWER
 # case "${ANSWER}" in
 #     [yY] | [yY][eE][sS])
@@ -98,14 +98,14 @@
 #     ;;
 # esac
 
-#Simple For loop
+## Simple For loop
 # NAMES="Brad Kevin Alice Mark"
 # for NAME in ${NAMES}
 #     do 
 #         echo "Hello, ${NAME}"
 # done
 
-#For loop to rename files
+## For loop to rename files
 # FILES=$(ls *.txt)
 # NEW="new"
 # for FILE in ${FILES}
@@ -114,7 +114,7 @@
 #         mv ${FILE} ${NEW}-${FILE}
 # done
 
-#While Loop -Read through A file line by line
+## While Loop -Read through A file line by line
 # LINE=1
 # while read -r CURRENT_LINE
 #     do
@@ -122,21 +122,21 @@
 #         ((LINE++))
 # done < "./new-Test.txt"
 
-#Function
+## Function
 # function sayHello(){
 #     echo "Hello World!"
 # }
 
 # sayHello
 
-#Function with params
+## Function with params
 # function greet(){
 #     echo "Hello, I am $1 and I am $2"
 # }
 
 # greet "Brad" "26"
 
-# create folder and write to a file
+## create folder and write to a file
 # mkdir hello 
 # touch "hello/world.txt"
 # echo "Hello World" > "hello/world.txt"
